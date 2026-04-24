c674bf1e71e530db970fccf80ad23557fb81f1d6
Stephanie A. Goulet <vthomeschoolmom@gmail.com>
2026-04-24 06:56:14 -0500

    First FlaUI element working

--- RoverExtras/FlaUI/Element.cs
+++ RoverExtras/FlaUI/Element.cs
@@ -2,6 +2,7 @@
 using FlaUI.Core.Conditions;
 using FlaUI.UIA3;
 using RoverTest.ModelUserInterface;
+using System.Reflection;
 
 namespace RoverExtras.FlaUI
 {
@@ -14,9 +15,7 @@ namespace RoverExtras.FlaUI
 
         public Element(FlaUIAppDriver flauiAppDriver)
         {
-            PropertyCondition condition = cf.ByAutomationId("40");
-            AutomationElement foundElement = flauiAppDriver.MainWindow.FindFirstDescendant(condition);
-            _automationElement = foundElement;
+            _automationElement = FindElement(flauiAppDriver);
         }
 
         public bool Visible => _automationElement?.IsAvailable == true &&
@@ -36,7 +35,72 @@ namespace RoverExtras.FlaUI
 
         public string Text => _automationElement.AsLabel()?.Text ?? string.Empty;
 
-        
-        
+        private AutomationElement FindElement(FlaUIAppDriver flauiAppDriver)
+        {
+            // Get the stack trace to find the calling property
+            var stackTrace = new System.Diagnostics.StackTrace();
+
+            // Walk up the stack to find the property that has the FlaUIFinder attribute
+            for (int i = 0; i < stackTrace.FrameCount; i++)
+            {
+                var method = stackTrace.GetFrame(i)?.GetMethod();
+                if (method == null) continue;
+
+                // Check if this is a property getter
+                if (method.Name.StartsWith("get_"))
+                {
+                    var propertyName = method.Name.Substring(4); // Remove "get_" prefix
+                    var declaringType = method.DeclaringType;
+
+                    if (declaringType != null)
+                    {
+                        var property = declaringType.GetProperty(propertyName,
+                            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+
+                        if (property != null)
+                        {
+                            var finderAttr = property.GetCustomAttribute<FlaUIFinder>();
+                            if (finderAttr != null)
+                            {
+                                return FindElementByAttribute(flauiAppDriver, finderAttr);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Fallback to default if no attribute found
+            PropertyCondition condition = cf.ByAutomationId("40");
+            return flauiAppDriver.MainWindow.FindFirstDescendant(condition);
+        }
+
+        private AutomationElement FindElementByAttribute(FlaUIAppDriver flauiAppDriver, FlaUIFinder finder)
+        {
+            ConditionBase condition = null;
+
+            // Build condition based on SearchTech
+            switch (finder.SearchTech)
+            {
+                case FlauiSearchTech.ByAutomationId:
+                    condition = cf.ByAutomationId(finder.FinderString);
+                    break;
+                case FlauiSearchTech.ByName:
+                    condition = cf.ByName(finder.FinderString);
+                    break;
+                case FlauiSearchTech.ByClassName:
+                    condition = cf.ByClassName(finder.FinderString);
+                    break;
+                case FlauiSearchTech.ByXPath:
+                    // XPath requires different handling
+                    return flauiAppDriver.MainWindow.FindFirstByXPath(finder.FinderString);
+                default:
+                    condition = cf.ByAutomationId(finder.FinderString);
+                    break;
+            }
+
+            return flauiAppDriver.MainWindow.FindFirstDescendant(condition);
+        }
+
+
     }
 }
