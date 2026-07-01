dc1413d7285e9891b4f8ab7739af6ad829a65866
Stephanie A. Goulet <vthomeschoolmom@gmail.com>
2026-07-01 09:50:39 -0500

    gonna pivot to selenium implentation to proceed with rover design and will tackle playwright after the coursework I have to do anyway.

--- RoverExtras.Playwright/Textbox.cs
+++ RoverExtras.Playwright/Textbox.cs
@@ -1,4 +1,5 @@
-﻿using RoverTest.ModelUserInterface;
+﻿using Microsoft.Playwright;
+using RoverTest.ModelUserInterface;
 
 namespace RoverExtras.Playwright
 {
@@ -7,7 +8,27 @@ namespace RoverExtras.Playwright
     {
         public void SendKeys(string entryValue)
         {
+            Locator.WaitForAsync(new LocatorWaitForOptions
+            {
+                State = WaitForSelectorState.Visible,
+                Timeout = 5000
+            }).GetAwaiter().GetResult();
+            //*** End wait for element ***
+
+            //*** Clear existing value before filling ***
+            Locator.ClearAsync().GetAwaiter().GetResult();
+            //*** End clear ***
+
+            // Fill the textbox with the new value
             Locator.FillAsync(entryValue).GetAwaiter().GetResult();
+
+            //*** Verify the value was actually set ***
+            var actualValue = Locator.InputValueAsync().GetAwaiter().GetResult();
+            if (actualValue != entryValue)
+            {
+                throw new InvalidOperationException(
+                    $"Failed to set textbox value. Expected: '{entryValue}', Actual: '{actualValue}'");
+            }
         }
     }
 }
